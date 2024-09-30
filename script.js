5e3b2ed297cf24ded89ee3b3eda3c3b874465d59
zikameki <149063060+zikameki@users.noreply.github.com>
2024-09-30 11:38:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 document.getElementById('pinForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Mencegah form submit
+    event.preventDefault(); // Mencegah form submit default
 
     const correctPin = "051206"; // PIN yang benar
     const enteredPin = document.getElementById('pin').value;
+    const errorMessage = document.getElementById('error-message');
 
     if (enteredPin === correctPin) {
         // Redirect ke halaman eksternal jika PIN benar
         window.location.href = "https://zikameki.github.io/gotri/"; // Ganti dengan URL tujuan
     } else {
         // Tampilkan pesan kesalahan jika PIN salah
-        document.getElementById('error-message').style.display = "block";
+        errorMessage.style.display = "block";
+        
+        // Setelah 3 detik, sembunyikan pesan kesalahan
+        setTimeout(function() {
+            errorMessage.style.display = "none";
+            // Reset input field setelah beberapa detik
+            document.getElementById('pin').value = "";
+        }, 3000); // 3000 milidetik = 3 detik
     }
 });
-
-
